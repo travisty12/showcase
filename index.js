46781697cfb30f6826314502dcb7633a17decb3f
Travis Scott <travisty12@gmail.com>
2020-12-18 15:26:46 -0800

    add basic server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/portfolio`);
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
