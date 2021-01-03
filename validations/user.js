45a6351f35fffc8ed85b1eb66bfffc1eb7a90748
Travis Scott <travisty12@gmail.com>
2021-01-03 15:25:15 -0800

    create user validations

---
+++ validations/user.js
@@ -0,0 +1,29 @@
+import Joi from 'joi';
+
+const email = Joi.string().email().required();
+const username = Joi.string().alphanum().min(3).max(30).required();
+
+const message = 'must be between 8 and 20 characters, ' + 
+  'have one capital letter, ' +
+  'one lowercase letter, ' +
+  'one number, ' +
+  'and one special character';
+
+const password = Joi.string().regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/).options({ language: {
+  string: {
+    regex: {
+      base: message
+    }
+  }
+}} );
+
+export const signUp = Joi.object().keys({
+  email,
+  username,
+  password
+});
+
+export const signIn = Joi.object().keys({
+  email,
+  password
+});
