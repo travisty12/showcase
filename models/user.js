b9498b29e8b00f5791cd6d377ace7fa5ae101b3e
Travis Scott <travisty12@gmail.com>
2021-01-03 15:11:40 -0800

    add user methods for hashing/validating fields/hash matching

--- models/user.js
+++ models/user.js
@@ -22,5 +22,19 @@ const UserSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
+UserSchema.pre('save', function() {
+  if (this.isModified('password')) {
+    this.password = hashSync(this.password, 10);
+  }
+});
+
+UserSchema.statics.doesNotExist = async function (field) {
+  return await this.where(field).countDocuments() === 0;
+};
+
+UserSchema.methods.comparePasswords = function(password) {
+  return compareSync(password, this.password);
+};
+
 const User = mongoose.model('User', UserSchema);
 export default User;
