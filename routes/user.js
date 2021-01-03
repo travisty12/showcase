29c4ea1465317d5bf32fb8b85cbdff3b450c0e68
Travis Scott <travisty12@gmail.com>
2021-01-03 15:28:10 -0800

    add route to post new user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,21 @@
 import express from "express";
+import Joi from 'joi';
+import User from '../models/user';
+import { signUp } from '../validations/user';
 
 const userRoutes = express.Router();
 
 userRoutes.post("", (req, res) => {
-  res.send(req.body);
+  try {
+    const { username, email, password } = req.body;
+    await Joi.validate({ username, email, password }, signUp);
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.send({ userId: newUser.id, username });
+
+  } catch (error) {
+    res.status(400).send(err);
+  }
 });
 
 export default userRoutes;
